7b74493f46a5d8274037937ce8d2c7f3c1b31fd3
Dominik Grzelak <grzelak.dominik@gmail.com>
2020-12-02 22:32:47 +0100

    WIP day 2 closure

---
+++ day2/clojure/project.clj
@@ -0,0 +1,3 @@
+(defproject day2 "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns day2.core})
---
+++ day2/clojure/src/day2/core.clj
@@ -0,0 +1,21 @@
+(ns day2.core)
+(use 'clojure.java.io)
+
+(defn is-valid
+  [password]
+  true)
+
+(defn validate-line
+  [line]
+  (def matches (re-find #"(\d+)-(\d+) (\w): (\w+)" line))
+  (def password {:value           (nth matches 4)
+                 :character       (nth matches 3)
+                 :min-occurrences (nth matches 1)
+                 :max-occurrences (nth matches 2)})
+  (is-valid password))
+
+(defn -main [& args]
+  (with-open [rdr (reader (clojure.java.io/resource "day2.txt"))]
+    (def validations
+      (map validate-line (line-seq rdr)))
+    (println validations)))
